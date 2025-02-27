827ad8a983c888d4cd71e89a0d1d2e48c034a342
Pedro <pedroaugustof.cardoso@gmail.com>
2025-02-27 09:41:12 -0300

    Roteamento de rota2

--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Cadastro from './Pages/Cadastro';
-import Login from './Pages/Login'; // ✅ Mantém apenas essa importação
+import Login from './Pages/Login';
 import { AuthProvider } from './AuthContext';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <Router> {/* ✅ Adiciona basename para GitHub Pages */}
+      <Router> 
         <Routes>
-          <Route path="/" element={<Login />} />   {/* ✅ Página inicial corrigida */}
-          <Route path="cadastro" element={<Cadastro />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
           <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
